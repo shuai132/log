6da09fe73f505982adc88f6f426377c1c295b0de
liushuai <770722922@qq.com>
2019-08-03 15:07:19 +0800

    feature: add project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # LOG
+
+[![Build Status](https://www.travis-ci.org/shuai132/LOG.svg?branch=master)](https://www.travis-ci.org/shuai132/LOG)
---
+++ example.c
@@ -0,0 +1,9 @@
+#include "log.h"
+
+int main() {
+    LOGD("DEBUG");
+    LOGI("LOGI");
+    LOGT("TAG", "LOGT");
+    LOGE("ERROR");
+    return 0;
+}
