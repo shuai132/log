e1135f54715302082351b2bde40afe0f4d84af21
liushuai <770722922@qq.com>
2019-08-21 13:33:18 +0800

    feature: make error red

--- log.h
+++ log.h
@@ -6,7 +6,7 @@
 // LOGI: INFO   会输出文件名信息
 // LOGE: ERROR  错误信息 会有ERROR:前缀
 // LOGD: DEBUG  debug模式(NDEBUG未定义)以LOGI输出 release模式无输出
-// 注意: 为了保证输出顺序 错误也使用stdout而不用stderr
+// 注意: 为了保证输出顺序 错误也使用stdout而不用stderr 输出为红色
 //
 
 #pragma once
@@ -26,7 +26,8 @@
 #define LOGF(fmt, ...)          do{ printf(fmt "\n", ##__VA_ARGS__); } while(0)
 #define LOGT(tag, fmt, ...)     do{ printf(tag ": " fmt "\n", ##__VA_ARGS__); } while(0)
 #define LOGI(fmt, ...)          do{ fprintf(stdout, "%s: " fmt "\n", __FILENAME__, ##__VA_ARGS__); } while(0)
-#define LOGE(fmt, ...)          do{ fprintf(stdout, "ERROR: %s: %s: %d: " fmt "\n", __FILENAME__, __func__, __LINE__, ##__VA_ARGS__); \
+#define LOGE(fmt, ...)          do{ fprintf(stdout, "\033[31mERROR: %s: %s: %d: " fmt "\n\033[m", \
+                                    __FILENAME__, __func__, __LINE__, ##__VA_ARGS__); \
                                 } while(0)
 
 #ifdef NDEBUG
