c59aead114d4892170ce3976fd119a15fc7d3e13
liushuai <770722922@qq.com>
2019-08-29 02:54:31 +0800

    feature: set LOGD to yellow color

--- log.h
+++ log.h
@@ -1,12 +1,12 @@
 /**
  * 方便打印日志
  *
- * 宏    含义    作用
- * LOG:  LOG    等效于printf并换行
- * LOGT: TAG    第一个参数为tag
- * LOGI: INFO   带有文件名信息
- * LOGE: ERROR  错误信息 会有ERROR:前缀 输出为红色
- * LOGD: DEBUG  debug模式(NDEBUG未定义)以LOGI输出 release模式无输出
+ * 宏    含义    作用                       颜色
+ * LOG:  LOG    等效于printf并换行         默认色
+ * LOGT: TAG    第一个参数为tag            默认色
+ * LOGI: INFO   带有文件名信息             默认色
+ * LOGE: ERROR  错误信息 会有ERROR:前缀     红色
+ * LOGD: DEBUG  debug模式（NDEBUG未定义时） 黄色
  * 为了保证输出顺序 错误也使用stdout而不是stderr
  */
 
@@ -27,12 +27,12 @@
 #define LOG(fmt, ...)           do{ printf(fmt "\n", ##__VA_ARGS__); } while(0)
 #define LOGT(tag, fmt, ...)     do{ printf(tag ": " fmt "\n", ##__VA_ARGS__); } while(0)
 #define LOGI(fmt, ...)          do{ printf("%s: " fmt "\n", __FILENAME__, ##__VA_ARGS__); } while(0)
-#define LOGE(fmt, ...)          do{ printf("\033[31mERROR: %s: %s: %d: " fmt "\n\033[m", \
+#define LOGE(fmt, ...)          do{ printf("\033[31mERROR: %s: %s: %d: " fmt "\033[m\n", \
                                     __FILENAME__, __func__, __LINE__, ##__VA_ARGS__); \
                                 } while(0)
 
 #ifdef NDEBUG
-#define LOGD(fmt, ...)  ((void)0)
+#define LOGD(fmt, ...)          ((void)0)
 #else
-#define LOGD(fmt, ...)  LOGI(fmt, ##__VA_ARGS__)
+#define LOGD(fmt, ...)          do{ printf("\033[33m%s: " fmt "\033[m\n", __FILENAME__, ##__VA_ARGS__); } while(0)
 #endif
