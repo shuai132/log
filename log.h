84e14d4da383e2670149b5008afe9dc87c60b5ab
liushuai <770722922@qq.com>
2019-12-02 15:15:10 +0800

    chore: refine code
    refine windows support
    add \r\n option

--- log.h
+++ log.h
@@ -12,25 +12,47 @@
 
 #pragma once
 
+#ifdef __cplusplus
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#else
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#endif
 
-// Truncates the full __FILE__ path, only displaying the basename
-#define __FILENAME__ \
-    (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
+#ifdef  LOG_LINE_END_CRLF
+#define LOG_LINE_END        "\r\n"
+#else
+#define LOG_LINE_END        "\n"
+#endif
 
-#define LOG(fmt, ...)           do{ printf(fmt "\n", ##__VA_ARGS__); } while(0)
-#define LOGT(tag, fmt, ...)     do{ printf(tag ": " fmt "\n", ##__VA_ARGS__); } while(0)
-#define LOGI(fmt, ...)          do{ printf("%s: " fmt "\n", __FILENAME__, ##__VA_ARGS__); } while(0)
-#define LOGE(fmt, ...)          do{ printf("\033[31mERROR: %s: %s: %d: " fmt "\033[m\n", \
-                                    __FILENAME__, __func__, __LINE__, ##__VA_ARGS__); \
+#ifndef _WIN32
+#define LOG_COLOR_RED_S     "\033[31m"
+#define LOG_COLOR_RED_E     "\033[31m"  LOG_LINE_END
+#define LOG_COLOR_YELLOW_S  "\033[33m"
+#define LOG_COLOR_YELLOW_E  "\033[33m"  LOG_LINE_END
+#define LOG_BASE_FILENAME   (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+#else
+#define LOG_COLOR_RED_S
+#define LOG_COLOR_RED_E     LOG_LINE_END
+#define LOG_COLOR_YELLOW_S
+#define LOG_COLOR_YELLOW_E  LOG_LINE_END
+#define LOG_BASE_FILENAME   (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
+#endif
+
+#define LOG(fmt, ...)           do{ printf(fmt LOG_LINE_END, ##__VA_ARGS__); } while(0)
+#define LOGT(tag, fmt, ...)     do{ printf(tag ": " fmt LOG_LINE_END, ##__VA_ARGS__); } while(0)
+#define LOGI(fmt, ...)          do{ printf("%s: " fmt LOG_LINE_END, LOG_BASE_FILENAME, ##__VA_ARGS__); } while(0)
+#define LOGE(fmt, ...)          do{ printf(LOG_COLOR_RED_S "ERROR: %s: %s: %d: " fmt LOG_COLOR_RED_E, \
+                                    LOG_BASE_FILENAME, __func__, __LINE__, ##__VA_ARGS__); \
                                 } while(0)
 
 #ifdef NDEBUG
 #define LOGD(fmt, ...)          ((void)0)
 #else
-#define LOGD(fmt, ...)          do{ printf("\033[33m%s: " fmt "\033[m\n", __FILENAME__, ##__VA_ARGS__); } while(0)
+#define LOGD(fmt, ...)          do{ printf(LOG_COLOR_YELLOW_S "%s: " fmt LOG_COLOR_YELLOW_E, LOG_BASE_FILENAME, ##__VA_ARGS__); } while(0)
 #endif
 
 #define FATAL(fmt, ...)         do{ LOGE(fmt, ##__VA_ARGS__); exit(EXIT_FAILURE); } while(0)
